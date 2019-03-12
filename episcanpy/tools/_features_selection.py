e94dd22d27f8560d564405d849f49560022502b5
Anna Danese <anna.danese@helmholtz-muenchen.de>
2019-03-12 13:49:51 +0100

    Update _features_selection.py

--- episcanpy/tools/_features_selection.py
+++ episcanpy/tools/_features_selection.py
@@ -2,7 +2,7 @@ import scanpy.api as sc
 
 def rank_features(adata, groupby, use_raw=True, groups='all', reference='rest', n_genes=100,
                      rankby_abs=False, key_added=None, copy=False, method='',
-                     corr_method='benjamini-hochberg', **kwds)
+                     corr_method='benjamini-hochberg', **kwds):
 
     if (method!='') and (adata.uns['omic'] != 'methylation'):
         method='t-test_overestim_var'
