2813ffbbd6f9ed7d12ab0aea24120aa9277ab4db
Anna Danese <anna.danese@helmholtz-muenchen.de>
2019-03-13 18:38:28 +0100

    Update _quality_control.py

--- episcanpy/preprocessing/_quality_control.py
+++ episcanpy/preprocessing/_quality_control.py
@@ -1,6 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.axes as pltax
+import numpy as np
 import anndata as ad
 
 def coverage_cells(adata, bins=50, key_added=None):
